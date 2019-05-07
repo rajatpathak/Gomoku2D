272237073b555ed2051fb2d6699e5fc521c00194
love <love@loves-MacBook-Pro.local>
2019-05-07 14:22:08 +0530

    first commit

---
+++ Assets/_Scripts/resultatIA.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class resultatIA : MonoBehaviour
+{
+
+    public Button ReplayButton;
+    public Button ExitButton;
+    public ControlIA control;
+    public Text Winner;
+
+    void Start()
+    {
+        Winner.text = ("Le Joueur " + control.playerwin + " gagne");
+        ExitButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Menu");
+        });
+        ReplayButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("GomokuIA");
+        });
+    }
+}
